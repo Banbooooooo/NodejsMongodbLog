26dce2e4e6ba5f3aec4c6d19a5908c8bf51f91d2
Banbooooooo <w190110910528@foxmail.com>
2021-12-08 16:59:32 -0800

    First commit

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
-app.get("/",(req,res)=>{
-    res.send("hello");
-})
+app.use('/',express.static('public'))
+// app.get("/",(req,res)=>{
+//     res.send("hello");
+// })
 app.listen(65530)
