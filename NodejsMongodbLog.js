fec9b460c7fb70cdc1717961c5984ed39ae2864c
Banbooooooo <w190110910528@foxmail.com>
2021-12-09 03:36:21 -0800

    second commit

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,8 +1,21 @@
 const express=require("express")
+const mongoose = require('mongoose')
 const app=express()
+
+
+mongoose.connect('mongodb://localhost/test1');
+const schema = {
+    name:String,
+    age:Number,
+    health:String
+}
+const mydata = mongoose.model('cat1s', schema);
+const kitty = new mydata({ name: 'testtr' });
+kitty.save()
+
 app.use('/',express.static('public'))
-// app.get("/input",(req,res)=>{
-//     res.send(req.query);
-//     console.log(req.query);
-// })
+app.get("/input",(req,res)=>{
+    res.send(req.query);
+    console.log(req.query);
+})
 app.listen(65530)
